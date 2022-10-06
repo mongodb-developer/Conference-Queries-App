1d24472c2b87c379ebfd069c72b97d43d5d328fc
Mohit Sharma <mohit7575@gmail.com>
2022-10-06 17:20:32 +0100

    repo cleanup and refactoring.

--- shared/src/commonMain/kotlin/com/mongodb/dublinmug_kmm/RealmRepo.kt
+++ shared/src/commonMain/kotlin/com/mongodb/dublinmug_kmm/RealmRepo.kt
@@ -3,74 +3,62 @@ package com.mongodb.dublinmug_kmm
 import CommonFlow
 import asCommonFlow
 import io.realm.kotlin.Realm
-import io.realm.kotlin.RealmConfiguration
 import io.realm.kotlin.ext.query
 import io.realm.kotlin.log.LogLevel
-import io.realm.kotlin.mongodb.App
-import io.realm.kotlin.mongodb.Credentials
-import io.realm.kotlin.mongodb.User
-import io.realm.kotlin.mongodb.subscriptions
+import io.realm.kotlin.mongodb.*
 import io.realm.kotlin.mongodb.sync.SyncConfiguration
-import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
 
 class RealmRepo {
 
-    private lateinit var realmOnDevice: Realm
+    lateinit var realm: Realm
 
-    private val realmApp by lazy { App.create("application-0-elgah") }
+    private val appServiceInstance by lazy {
+        // If logs are on app level then it set for everything ..
+        val configuration =
+            AppConfiguration.Builder("application-0-elgah").log(LogLevel.ALL).build()
+        App.create(configuration)
+    }
 
-    private val realm by lazy {
-        val user = realmApp.currentUser!!
+    private suspend fun setupRealmSync() {
+        val user = appServiceInstance.login(Credentials.anonymous())
         val config = SyncConfiguration
             .Builder(user, setOf(QueryInfo::class))
-            .log(LogLevel.ALL)
             .initialSubscriptions { realm ->
-                realm.query<QueryInfo>()
+                // only can write data, which cover in initialSubscriptions
+                add(
+                    query = realm.query<QueryInfo>(),
+                    name = "subscription name",
+                    updateExisting = true
+                )
             }
             .build()
-        Realm.open(config)
-    }
-
-    init {
-        realmOnDeviceSetup()
-    }
-
-    private fun realmOnDeviceSetup() {
-        val schemaClass = setOf(QueryInfo::class)
-
-        val config = RealmConfiguration
-            .Builder(schema = schemaClass)
-            .name("DubMug")
-            .schemaVersion(1)
-            .log(LogLevel.ALL)
-            .build()
-
-        realmOnDevice = Realm.open(config)
+        realm = Realm.open(config)
     }
 
     suspend fun saveInfo(query: String) {
-        realmApp.login(Credentials.anonymous())
-
-        realm.subscriptions.update {
-            this.add(
-                query = realm.query<QueryInfo>(),
-                name = "subscription name",
-                updateExisting = true
-            )
+        if (!this::realm.isInitialized) {
+            setupRealmSync()
         }
 
         val info = QueryInfo().apply {
             _id = RandomUUID().randomId
             queries = query
         }
-
         realm.write {
             copyToRealm(info)
         }
     }
 
-    fun getAllData(): CommonFlow<List<String>> {
+    suspend fun getAllData(): CommonFlow<List<String>> {
+        if (!this::realm.isInitialized) {
+            setupRealmSync()
+        }
         return realm.query<QueryInfo>().asFlow().map {
             it.list.map { it.queries }
         }.asCommonFlow()
