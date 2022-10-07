bb52181836156e759e4976855dc30ac2b1bc0e14
Christian Melchior <christian.melchior@mongodb.com>
2022-10-07 12:03:40 +0200

    Rework threading so ViewModels run from the Main thread only. The RealmRepo is then responsible for delegating to the background as needed. This makes the contract between app code and shared code simpler as it is only the RealmRepo that control dispatching work to different threads.

--- androidApp/src/main/java/com/mongodb/dublinmug_kmm/android/MainViewModel.kt
+++ androidApp/src/main/java/com/mongodb/dublinmug_kmm/android/MainViewModel.kt
@@ -1,16 +1,18 @@
 package com.mongodb.dublinmug_kmm.android
 
-import androidx.lifecycle.*
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.liveData
+import androidx.lifecycle.viewModelScope
 import com.mongodb.dublinmug_kmm.RealmRepo
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
 
     private val repo = RealmRepo()
     val queries: LiveData<List<String>> = liveData {
-        emitSource(repo.getAllData().flowOn(Dispatchers.IO).asLiveData(Dispatchers.Main))
+        emitSource(repo.getAllData().asLiveData())
     }
 
     fun saveQuery(query: String) {
