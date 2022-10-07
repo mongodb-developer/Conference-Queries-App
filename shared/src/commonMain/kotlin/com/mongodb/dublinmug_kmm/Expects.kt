50dd4187d8ac9b9c6c46140792587a2f1f6fafdd
Christian Melchior <christian.melchior@mongodb.com>
2022-10-07 12:00:00 +0200

    Use RealmUUID instead of custom implementation.

--- shared/src/commonMain/kotlin/com/mongodb/dublinmug_kmm/Expects.kt
+++ shared/src/commonMain/kotlin/com/mongodb/dublinmug_kmm/Expects.kt
@@ -1,9 +1,5 @@
 package com.mongodb.dublinmug_kmm
 
-expect class RandomUUID(){
-    val randomId : String
-}
-
 expect class Platform(){
     val getPlatform : String
 }
--- shared/src/iosMain/kotlin/com/mongodb/dublinmug_kmm/RandomUUID.kt
+++
@@ -1,9 +0,0 @@
-package com.mongodb.dublinmug_kmm
-
-import platform.Foundation.NSUUID
-
-actual class RandomUUID {
-
-    actual val randomId: String
-        get() = NSUUID.UUID().UUIDString
-}
