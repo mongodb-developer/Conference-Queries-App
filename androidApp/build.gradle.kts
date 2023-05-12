9fe9000166576c9f56119a2eb886b84bbd986ce9
Mohit Sharma <mohit7575@gmail.com>
2023-05-12 06:56:59 +0200

    new icon added

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.3.0"
+        kotlinCompilerExtensionVersion = "1.4.7"
     }
     packagingOptions {
         resources {
@@ -33,32 +33,23 @@ android {
 
 dependencies {
     implementation(project(":shared"))
-    implementation("androidx.compose.ui:ui:1.2.1")
-    implementation("androidx.compose.ui:ui-tooling:1.2.1")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.2.1")
-    implementation("androidx.compose.foundation:foundation:1.2.1")
+    implementation("androidx.compose.ui:ui:1.4.3")
+    implementation("androidx.compose.ui:ui-tooling:1.4.3")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.4.3")
+    implementation("androidx.compose.foundation:foundation:1.4.3")
 
     // Compose 
     // Integration with activities
-    implementation("androidx.activity:activity-compose:1.6.0")
+    implementation("androidx.activity:activity-compose:1.7.1")
     // Compose Material Design
     implementation("androidx.compose.material3:material3:1.0.0-rc01")
     // Animations
-    implementation("androidx.compose.animation:animation:1.2.1")
-    // Tooling support (Previews, etc.)
-    implementation("androidx.compose.ui:ui-tooling:1.2.1")
+    implementation("androidx.compose.animation:animation:1.4.3")
     // Integration with ViewModels
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
-    implementation ("androidx.compose.runtime:runtime-livedata:1.2.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+    implementation ("androidx.compose.runtime:runtime-livedata:1.4.3")
     // UI Tests
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.2.1")
-    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.5.1")
-
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0-native-mt") {
-        version {
-            strictly("1.6.0-native-mt")
-        }
-    }
-
-    compileOnly("io.realm.kotlin:library-sync:1.1.0")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.4.3")
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
+    compileOnly("io.realm.kotlin:library-sync:1.7.0")
 }
