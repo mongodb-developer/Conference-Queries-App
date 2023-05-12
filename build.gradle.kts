8815c844964ec3634200237607f3e7363ce21ee4
Mohit Sharma <mohit7575@gmail.com>
2023-05-12 06:59:13 +0200

    version updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,10 @@ plugins {
     id("com.android.application").version("7.3.0").apply(false)
     id("com.android.library").version("7.3.0").apply(false)
 
-    kotlin("android").version("1.7.10").apply(false)
-    kotlin("multiplatform").version("1.7.10").apply(false)
+    kotlin("android").version("1.8.21").apply(false)
+    kotlin("multiplatform").version("1.8.21").apply(false)
 
-    id("io.realm.kotlin") version "1.1.0"
+    id("io.realm.kotlin") version "1.8.0"
 }
 
 tasks.register("clean", Delete::class) {
