8addc223d059d9c220eb9701b7fa6db78001e2b6
Mohit Sharma <mohit7575@gmail.com>
2023-05-23 10:40:28 +0100

    UI screen update for iOS code.

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -2,44 +2,49 @@ import SwiftUI
 import shared
 
 struct ContentView: View {
-	
-    
+
+
     @State var queries = [QueryInfo]()
     @State var query:String = ""
 
     var repo = RealmRepo()
-    
-	var body: some View {
-        VStack{
 
-            
-            TextField("Enter Query", text: $query)
-                .multilineTextAlignment(.center)
-                .textFieldStyle(.roundedBorder)
+    var body: some View {
+
+        NavigationView {
+            VStack(spacing: 20){
+
+                TextField("Enter Query", text: $query)
+                    .multilineTextAlignment(.center)
+                    .textFieldStyle(.roundedBorder)
+                    .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
+
+                Button("Save"){
+                    save(txt: query)
+                }
                 .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
-                
-            
-            Button("Save"){
-                save(txt: query)
+                .foregroundColor(.white)
+                .background(Color.blue)
+                .clipShape(Capsule())
+
             }
-            
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarTitleDisplayMode(NavigationBarItem.TitleDisplayMode.large)
+            .navigationTitle("Ask your Query")
         }
-        .navigationBarBackButtonHidden(true)
-        .navigationBarTitle("Mongo Dub Event",displayMode: .inline)
     }
-    
+
     func save(txt: String){
         repo.saveInfo(query: txt) { _ in
             print("Error")
         }
     }
-    
 }
 
 
 
 struct ContentView_Previews: PreviewProvider {
-	static var previews: some View {
-		ContentView()
-	}
+    static var previews: some View {
+        ContentView()
+    }
 }
